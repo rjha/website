8b078826d52e87bce7996ccee515f952bdbeb619
Rajeev Jha <jha.rajeev@gmail.com>
2013-03-03 21:16:04 +0530

    unlink old compiled files

--- web/compiled/fragments/links.tmpl.cc
+++
@@ -1,9 +0,0 @@
-<h4> Most Recent </h4>
-<div class="feeds">
-	<?php foreach($view->links as $link){ ?>
-	<div class="item">
-  		<a href="<?php echo $link['seo_title'] ?>"> <?php echo $link['title'] ?> </a>
-	</div>
-	<?php } ?>
-	
-</div>
