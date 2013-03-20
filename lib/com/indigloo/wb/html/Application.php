1fd0bcdc12b03f4681caec3bc0e2c5b73f7c73ba
rjha <jha.rajeev@gmail.com>
2013-03-20 17:08:03 +0530

    + refactored css file  + added images rendering to widgets

--- lib/com/indigloo/wb/html/Application.php
+++ lib/com/indigloo/wb/html/Application.php
@@ -36,14 +36,54 @@ namespace com\indigloo\wb\html {
 
         static function getWidget($widgetDBRow) {
             $html = NULL ;
-            $template = "/fragments/widget/text.tmpl" ;
+            $template = "/fragments/widget/post.tmpl" ;
             $view = new \stdClass;
             
+            $view->hasImage = false ;
+            $view->images = array();
+            // get media
+            $imagesJson = $widgetDBRow["media_json"];
+            $images = json_decode($imagesJson);
+
+            if(!empty($images) && (sizeof($images) > 0)) {
+                $view->hasImage = true ;
+                
+
+                foreach($images as $image) {
+                    $imgv = self::convertImageJsonObj($image);
+                    array_push($view->images,$imgv);
+                }
+            } 
+
             $view->content = $widgetDBRow['widget_html'];
             $view->title = $widgetDBRow['title'];
            
             $html = Template::render($template,$view);
             return $html ;
+        }
+
+         static function convertImageJsonObj($jsonObj) {
+            $imgv = array();
+
+            $imgv["name"] = $jsonObj->originalName ;
+            $prefix = ($jsonObj->store == 's3') ? 'http://' : Url::base().'/' ;
+            $tfile = NULL ;
+
+            //@imp: thumbnail not available? use original image
+            if(property_exists($jsonObj,"thumbnailName")) {
+                $imgv["tname"] = $jsonObj->thumbnailName ;
+                $tfile = $jsonObj->thumbnail ;
+            } else {
+                $imgv["tname"] = $jsonObj->originalName ;
+                $tfile = $jsonObj->storeName ;
+            }
+
+            $imgv["source"] = $prefix.$jsonObj->bucket.'/'.$jsonObj->storeName;
+            $imgv["tsource"] = $prefix.$jsonObj->bucket.'/'.$tfile ;
+            $imgv["width"] = $jsonObj->width ;
+            $imgv["height"] = $jsonObj->height;
+            //@todo add thumbnail width and height?
+            return $imgv ;
         }
 
         static function getLinks($links) {
