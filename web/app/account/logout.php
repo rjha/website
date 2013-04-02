869218d296f5788f1e9fe765040e1eb0d70365a4
rjha <jha.rajeev@gmail.com>
2013-04-02 16:14:48 +0530

    + fixed logout page  + fixed admin.inc (revert to simple login not found page)

--- web/app/account/logout.php
+++ web/app/account/logout.php
@@ -2,24 +2,52 @@
 
     include ('wb-app.inc');
     include(APP_CLASS_LOADER);
-    include(APP_WEB_DIR . '/inc/session.inc');
-    include(WEBGLOO_LIB_ROOT . '/com/indigloo/error.inc');
+    
+    use \com\indigloo\Configuration as Config;
+    use \com\indigloo\wb\Constants as AppConstants;
+   
+    // DB connx is needed for mysql session.
+    $domain = $_SERVER["HTTP_HOST"];
+    $pos1 = strpos($domain,".");
+    $top_domain = substr($domain,$pos1 + 1);
+    
+    $gWeb = \com\indigloo\core\Web::getInstance();
+    $gWeb->start();
+    $gWeb->setRequestAttribute(AppConstants::SITE_TOP_DOMAIN,$top_domain);
 
-    //destroy session
+    // session_start() should be called in logout script
+    $session_backend = Config::getInstance()->get_value("session.backend");
+    $session_backend = empty($session_backend) ? "default" :  strtolower($session_backend);
+
+    switch($session_backend) {
+        case "mysql" :
+            include(APP_WEB_DIR . '/inc/mysql-session.inc');
+            break ;
+        default:
+            include(APP_WEB_DIR . '/inc/session.inc');
+            break ;
+    }
+    
+    include(APP_WEB_DIR . '/inc/global-error.inc');
+    
+
+    // destroy session
     $_SESSION = array();
-    session_destroy();
-
+    
     if (ini_get("session.use_cookies")) {
         $params = session_get_cookie_params();
         setcookie(session_name(), '', time() - 42000,
             $params["path"], $params["domain"],
             $params["secure"], $params["httponly"]
         );
+
+        setcookie (session_id(), "", time() - 3600);
     }
 
-
-    //redirect to a _session free page
-    //redirecting to HOME will start a new session
-    header('Location: /start.html');
+    session_destroy();
+    // redirect to a _session free page
+    // redirecting to a page that includes our standard
+    // header will start a new session
+    header('Location: /');
 
 ?>
